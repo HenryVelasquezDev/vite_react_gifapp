a1a38df5c3f596e0fd61bcadc54fafdb48e156af
henry.velasquez <henryalex25@gmail.com>
2022-11-17 13:27:00 -0500

    se actualiza url del api

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,7 +1,7 @@
 
 
 export const getGifs = async ( categoria) => {
-    const url = `http://api.giphy.com/v1/gifs/search?api_key=M1iKEqA5ZducbcPLAOrICrSj8Xb1yb8B&q=${categoria}&limit=10`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=M1iKEqA5ZducbcPLAOrICrSj8Xb1yb8B&q=${categoria}&limit=10`;
     const resp = await  fetch(url);
     const { data } = await resp.json();
 
